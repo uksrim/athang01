9f42ec40b660c76529653337c802ad76cba0f5c8
uksrim <ugyen_tshering@rim.edu.bt>
2024-05-20 10:40:04 +0600

    Temp: Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeTravel(),
-      //home: DiscoveryPlants(),
+      // home: HomeTravel(),
+      home: DiscoveryPlants(),
     );
   }
 }
